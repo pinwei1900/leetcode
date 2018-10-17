565951c05fd5ee875405616405b0ba11b9726e86
songlin <www.1124106217@qq.com>
2018-10-17 17:03:01 +0800

    update 24.swap-nodes-in-pairs.0.java

---
+++ 24.swap-nodes-in-pairs.0.java
@@ -0,0 +1,61 @@
+/*
+ * [24] Swap Nodes in Pairs
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (53.10%)
+ * Total Accepted:    6.4K
+ * Total Submissions: 11.9K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 
+ * 示例:
+ * 
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ * 
+ * 说明:
+ * 
+ * 
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode list =  head;
+        ListNode pre = head;
+        while (list != null && list.next != null) {
+            if (list == head) {
+                ListNode temp = list.next;
+                head.next = temp.next;
+                temp.next = head;
+                head = temp;
+                pre = head.next;
+                list = head.next.next;
+                continue;
+            }
+
+            ListNode temp = list.next;
+            pre.next = temp;
+            list.next = temp.next;
+            temp.next = list;
+            pre = temp.next;
+            list = temp.next.next;
+        }
+        return head;
+    }
+}
