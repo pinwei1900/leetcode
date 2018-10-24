f39d621bfeec2df144d9350598d5b2acdbd50087
songlin <www.1124106217@qq.com>
2018-10-24 23:18:44 +0800

    update 30.substring-with-concatenation-of-all-words.java

--- 30.substring-with-concatenation-of-all-words.java
+++ 30.substring-with-concatenation-of-all-words.java
@@ -37,30 +37,35 @@ import java.util.List;
  * 
  */
 class Solution {
-
-    public static List<Integer> findSubstring(String S, String[] L) {
-        List<Integer> res = new ArrayList<Integer>();
-        if (S == null || L == null || L.length == 0) return res;
-        int len = L[0].length(); // length of each word
-        
-        Map<String, Integer> map = new HashMap<String, Integer>(); // map for L
-        for (String w : L) map.put(w, map.containsKey(w) ? map.get(w) + 1 : 1);
-        
-        for (int i = 0; i <= S.length() - len * L.length; i++) {
-            Map<String, Integer> copy = new HashMap<String, Integer>(map);
-            for (int j = 0; j < L.length; j++) { // checkc if match
-                String str = S.substring(i + j*len, i + j*len + len); // next word
-                if (copy.containsKey(str)) { // is in remaining words
-                    int count = copy.get(str);
-                    if (count == 1) copy.remove(str);
-                    else copy.put(str, count - 1);
-                    if (copy.isEmpty()) { // matches
-                        res.add(i);
-                        break;
-                    }
-                } else break; // not in L
+    boolean accept(String[] words, String subStr, int len) {
+        String[] splits = new String[words.length];
+        for (int i = 0; i < words.length; i++) {
+            splits[i] = subStr.substring(i * len, (i + 1) * len);
+        }
+        Arrays.sort(words);
+        Arrays.sort(splits);
+        for (int i = 0; i < words.length; i++) {
+            if (!words[i].equals(splits[i])) {
+                return false;
             }
         }
-        return res;
+        return true;
+    }
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        if (words.length == 0) {
+            return new ArrayList();
+        }
+        List<Integer> result = new ArrayList<>();
+        int len = words[0].length() * words.length;
+        
+        int wordLen = words[0].length();
+        for (int i = 0; i <= s.length() - len; i++) {
+            String p = s.substring(i, i + len);
+            if (accept(words, p ,wordLen)) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
