a690e5c7ae798a724647f87350cb16d955e41a56
songlin <www.1124106217@qq.com>
2018-10-25 10:22:59 +0800

    update 31.next-permutation.java

---
+++ 31.next-permutation.java
@@ -0,0 +1,65 @@
+/*
+ * [31] Next Permutation
+ *
+ * https://leetcode-cn.com/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (27.48%)
+ * Total Accepted:    4.5K
+ * Total Submissions: 16.5K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * 
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 
+ * 必须原地修改，只允许使用额外常数空间。
+ * 
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * 
+ */
+class Solution {
+    void swap(int[] nums, int p1, int p2) {
+        int temp = nums[p1];
+        nums[p1] = nums[p2];
+        nums[p2] = temp;
+    }
+
+    public void nextPermutation(int[] nums) {
+        int len = nums.length;
+        if (len == 0)
+            return;
+
+        int i = len - 1;
+
+        boolean flag = false;
+        for (; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (i == 0) {
+            for (; i < len/2; i++) {
+                swap(nums ,i ,len -1 - i);
+            }
+            return;
+        }
+
+        int z = i;
+        for (int j = i; j < len; j++) {
+            if (nums[j] > nums[i - 1]) {
+                if (nums[j] < nums[z]) {
+                    z = j;
+                }
+            }
+        }
+
+        swap(nums, i - 1, z);
+        Arrays.sort(nums ,i ,len);
+    }
+}
